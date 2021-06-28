895d50dbe4bf1dec7d6f11c2093dac12ac40a608
mdazharuddin1011999 <mdazharuddin1011999@gmail.com>
2021-06-28 13:29:22 +0530

    Added extension asPokemonProfile

--- app/src/main/java/com/az/pokedex/model/remote/ResponseResult.kt
+++ app/src/main/java/com/az/pokedex/model/remote/ResponseResult.kt
@@ -1,6 +1,7 @@
 package com.az.pokedex.model.remote
 
 
+import com.az.pokedex.model.PokemonProfile
 import com.squareup.moshi.Json
 import com.squareup.moshi.JsonClass
 
@@ -11,3 +12,20 @@ data class ResponseResult(
     @Json(name = "url")
     val url: String
 )
+
+fun ResponseResult.asPokemonProfile(): PokemonProfile {
+    val name = this.name
+    val url = this.url
+    val id = if(url.endsWith("/")){
+        url.dropLast(1).takeLastWhile { it.isDigit() }
+    }else{
+        url.takeLastWhile { it.isDigit() }
+    }
+    val imageUrl = "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/$id.png"
+    return PokemonProfile(
+        id = id.toLong(),
+        name = name,
+        imageUrl = imageUrl,
+        favorite = false
+    )
+}
