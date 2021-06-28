58dbc79927c9f11983a214fdb74ef5d156da0470
mdazharuddin1011999 <mdazharuddin1011999@gmail.com>
2021-06-28 13:28:23 +0530

    Changed url to imageUrl

--- app/src/main/java/com/az/pokedex/model/PokemonProfile.kt
+++ app/src/main/java/com/az/pokedex/model/PokemonProfile.kt
@@ -3,6 +3,7 @@ package com.az.pokedex.model
 import androidx.room.ColumnInfo
 import androidx.room.Entity
 import androidx.room.PrimaryKey
+import com.az.pokedex.model.remote.ResponseResult
 
 @Entity(tableName = "pokemon_list")
 data class PokemonProfile(
@@ -12,8 +13,8 @@ data class PokemonProfile(
     @ColumnInfo(name = "name")
     val name: String,
 
-    @ColumnInfo(name = "url")
-    val url: String,
+    @ColumnInfo(name = "image_url")
+    val imageUrl: String,
 
     @ColumnInfo(name = "favorite")
     val favorite: Boolean,
