bd9f60da2503938d1ece0bfa3ec8a5cbb84413b8
mdazharuddin1011999 <mdazharuddin1011999@gmail.com>
2021-06-28 13:58:03 +0530

    Added NavHost

--- app/src/main/java/com/az/pokedex/ui/Pokedex.kt
+++ app/src/main/java/com/az/pokedex/ui/Pokedex.kt
@@ -14,24 +14,40 @@ import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
+import androidx.navigation.NavHostController
+import androidx.navigation.NavType
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.navArgument
+import androidx.navigation.compose.rememberNavController
 import com.az.pokedex.model.PokemonProfile
+import com.az.pokedex.ui.view.pokemondetail.PokemonDetailView
+import com.az.pokedex.ui.view.pokemonlist.PokemonListView
 
 @Composable
-fun Pokedex(
-    viewModel: PokedexViewModel = hiltViewModel()
-) {
-    val pokemonList: List<PokemonProfile> by viewModel.pokemonList.observeAsState(listOf())
-    LazyColumn {
-        items(pokemonList){ pokemon ->
-            Row(
-                horizontalArrangement = Arrangement.SpaceBetween,
-                modifier = Modifier
-                    .fillMaxWidth()
-                    .padding(horizontal = 24.dp)
-            ) {
-                Text(pokemon.id.toString())
-                Text(pokemon.name)
-            }
+fun Pokedex() {
+    val navController: NavHostController = rememberNavController()
+
+    NavHost(
+        navController = navController,
+        startDestination = "/pokemon_list"
+    ){
+        composable(
+            route = "/pokemon_list"
+        ){
+            PokemonListView()
+        }
+
+        composable(
+            route = "/pokemon_detail/{pokemonId}/{dominantColor}",
+            arguments = listOf(
+                navArgument("pokemonId"){ type = NavType.LongType },
+                navArgument("dominantColor"){ type = NavType.IntType },
+            )
+        ){
+            val pokemonId: Long = it.arguments?.getLong("pokemonId") ?: 1
+            val dominantColor: Int = it.arguments?.getInt("dominantColor") ?: 0
+            PokemonDetailView(pokemonId, dominantColor)
         }
     }
 }
