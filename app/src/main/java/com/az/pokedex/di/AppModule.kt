676377f86452a64702097b472fc31c9c036b7451
mdazharuddin1011999 <mdazharuddin1011999@gmail.com>
2021-06-28 13:27:16 +0530

    Made providers singleton and install-in singletonComponent

--- app/src/main/java/com/az/pokedex/di/AppModule.kt
+++ app/src/main/java/com/az/pokedex/di/AppModule.kt
@@ -3,21 +3,26 @@ package com.az.pokedex.di
 import com.az.pokedex.repository.pokemon.DefaultPokemonRepository
 import com.az.pokedex.repository.pokemon.PokemonRepository
 import com.az.pokedex.service.PokeApi
+import com.az.pokedex.service.database.PokemonDao
 import com.az.pokedex.utils.Constants.BASE_URL
 import com.squareup.moshi.Moshi
 import com.squareup.moshi.kotlin.reflect.KotlinJsonAdapterFactory
+import dagger.Binds
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.android.components.ActivityComponent
+import dagger.hilt.components.SingletonComponent
 import retrofit2.Retrofit
 import retrofit2.converter.moshi.MoshiConverterFactory
+import javax.inject.Singleton
 
 @Module
-@InstallIn(ActivityComponent::class)
+@InstallIn(SingletonComponent::class)
 object AppModule {
     @Provides
-    fun providePokeApi(): PokeApi{
+    @Singleton
+    fun providePokeApi(): PokeApi {
         val moshi = Moshi.Builder()
             .add(KotlinJsonAdapterFactory())
             .build()
@@ -30,8 +35,17 @@ object AppModule {
     }
 
     @Provides
+    @Singleton
     fun providePokemonRepository(
-        pokeApi: PokeApi
-    ): PokemonRepository = DefaultPokemonRepository(pokeApi)
+        pokeApi: PokeApi,
+        pokemonDao: PokemonDao
+    ): PokemonRepository = DefaultPokemonRepository(pokeApi, pokemonDao)
 
 }
+
+//@Module
+//@InstallIn(ActivityComponent::class)
+//abstract class PokemonRepositoryModule{
+//    @Binds
+//    abstract fun bindPokemonRepository(impl: DefaultPokemonRepository): PokemonRepository
+//}
--- app/src/main/java/com/az/pokedex/di/DatabaseModule.kt
+++ app/src/main/java/com/az/pokedex/di/DatabaseModule.kt
@@ -9,10 +9,11 @@ import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.android.components.ActivityComponent
 import dagger.hilt.android.qualifiers.ApplicationContext
+import dagger.hilt.components.SingletonComponent
 import javax.inject.Singleton
 
 @Module
-@InstallIn(ActivityComponent::class)
+@InstallIn(SingletonComponent::class)
 object DatabaseModule {
 
     @Provides
@@ -30,6 +31,7 @@ object DatabaseModule {
     }
 
     @Provides
+    @Singleton
     fun providePokemonDao(
         pokemonDatabase: PokemonDatabase
     ): PokemonDao = pokemonDatabase.pokemonDao()
